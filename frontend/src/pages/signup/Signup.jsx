242f0c57b14db9411c837cbc7634a346bb7a4a4d
messatrt <essaessa9068@gmial.com>
2025-03-15 19:36:23 +0530

    resolved merge conflicts

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -19,18 +19,16 @@ export default function Signup() {
           <div className="input-group">
             <label htmlFor="email-input">Email</label>
 
-            <input className="login-input" type="email" id="email-input" placeholder="Enter your email" />
-            
-            <label htmlFor="password-input">Password</label>
-            <input className="login-input" type="password" id="password-input" placeholder="Enter your password" />
-
             <input
+              className="login-input"
               type="email"
               id="email-input"
               placeholder="Enter your email"
             />
+
             <label htmlFor="password-input">Password</label>
             <input
+              className="login-input"
               type="password"
               id="password-input"
               placeholder="Enter your password"
